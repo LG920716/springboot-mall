c2a5fa8107767ef4eb6620c2a27d935e971442ab
intern-rayhuang <egroup.intern.rayhuang@gmail.com>
2023-07-19 11:09:47 +0800

    findProductById completed

--- spring-mail/src/main/java/com/ray/springmail/controller/ProductController.java
+++ spring-mail/src/main/java/com/ray/springmail/controller/ProductController.java
@@ -4,7 +4,6 @@ import com.ray.springmail.entity.Product;
 import com.ray.springmail.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
